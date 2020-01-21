af1d3f21e4219c018d330675b5455017f6afd4dd
hashmu <39256543+hashmu@users.noreply.github.com>
2020-01-21 14:08:19 +0800

    Add project files.

---
+++ HashLauncher/Model/AppLibrary.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace HashLauncher.Model
+{
+    public class AppLibrary
+    {
+        public List<App> apps = new List<App>();
+
+        public AppLibrary()
+        {
+            //Form1.player.SoundLocation = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "opensound.wav");
+        }
+
+        public class App
+        {
+            public string name;
+            public string path;
+            public string args;
+
+            public void Open()
+            {
+                try
+                {
+                    //Form1.player.Play();
+                    if (string.IsNullOrWhiteSpace(args))
+                    {
+                        Process.Start(@path);
+                    }
+                    else
+                    {
+                        ProcessStartInfo startInfo = new ProcessStartInfo();
+                        startInfo.FileName = path;
+                        startInfo.Arguments = args;
+                        Process.Start(startInfo);
+                    }
+                }
+                catch
+                {
+                    System.Windows.MessageBox.Show("Something went wrong");
+                }
+            }
+
+            public void Clicked(object sender, EventArgs args)
+            {
+                Open();
+            }
+        }
+    }
+}
---
+++ HashLauncher/TaskbarControl.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Hardcodet.Wpf.TaskbarNotification;
+using HashLauncher.Model;
+
+namespace HashLauncher
+{
+    class TaskbarControl
+    {
+        public TaskbarControl()
+        {
+            ReadApps();
+        }
+        
+        public AppLibrary appLibrary = new AppLibrary();
+        public ContextMenu contextMenu
+        {
+            get
+            {
+                return MainWindow.instance.myNotifyIcon.ContextMenu;
+            }
+            set
+            {
+                MainWindow.instance.myNotifyIcon.ContextMenu = value;
+            }
+        }
+
+        public void ReadApps()
+        {
+            try
+            {
+                string filepath = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "apps.txt");
+                string[] raw = File.ReadAllLines(filepath);
+                appLibrary.apps.Clear();
+                foreach (string line in raw)
+                {
+                    string[] split = line.Split(',');
+                    if (split.Length > 1)
+                    {
+                        AppLibrary.App newApp = new AppLibrary.App()
+                        {
+                            name = split[0],
+                            path = split[1]
+                        };
+                        if (split.Length > 2)
+                        {
+                            newApp.args = split[2];
+                        }
+                        appLibrary.apps.Add(newApp);
+                    }
+                }
+
+                contextMenu = new ContextMenu();
+                contextMenu.Items.Clear();
+                for (int i = 0; i < appLibrary.apps.Count; i++)
+                {
+                    MenuItem newitem = new MenuItem();
+                    newitem.Header = appLibrary.apps[i].name;
+                    newitem.Click += appLibrary.apps[i].Clicked;
+                    contextMenu.Items.Add(newitem);
+                }
+            }
+            catch
+            {
+                contextMenu = new ContextMenu();
+                contextMenu.Items.Clear();
+            }
+            finally
+            {
+                contextMenu.Items.Add(new Separator());
+                
+                MenuItem refresh = new MenuItem();
+                refresh.Header = "Refresh List";
+                refresh.Click += RefreshHandler;
+                contextMenu.Items.Add(refresh);
+
+                MenuItem edit = new MenuItem();
+                edit.Header = "Edit List";
+                edit.Click += EditListHandler;
+                contextMenu.Items.Add(edit);
+
+                MenuItem close = new MenuItem();
+                close.Header = "Close";
+                close.Click += CloseHandler;
+                contextMenu.Items.Add(close);
+            }
+        }
+
+        public void EditListHandler(object sender, EventArgs args)
+        {
+            //System.Diagnostics.Process.Start(@"apps.txt");
+
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.FileName = @"explorer.exe";
+            startInfo.Arguments = @"apps.txt";
+            System.Diagnostics.Process.Start(startInfo);
+        }
+
+        public void RefreshHandler(object sender, EventArgs args)
+        {
+            ReadApps();
+        }
+
+        public void CloseHandler(object sender, EventArgs args)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+    }
+}
